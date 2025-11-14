b9694cc8f9b60bf1a78b270cbe2aa3f491ddb0d6
yatharth kundra <82939639+yatharthk@users.noreply.github.com>
2025-11-14 15:09:50 +0530

    Remediated code

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchComponent } from './search.component';
+import { FormsModule } from '@angular/forms';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -8,7 +8,8 @@ describe('SearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [ SearchComponent ],
+      imports: [FormsModule]
     })
     .compileComponents();
   }));
