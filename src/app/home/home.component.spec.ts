80e4497492f5cce20f54f95527f4174ad65bb899
yatharth kundra <82939639+yatharthk@users.noreply.github.com>
2025-11-23 20:42:11 +0530

    Remediated code

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +8,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -11,7 +11,7 @@ export class VideoListComponent implements OnInit,OnDestroy {
 
   req:any;
   // someItem="<h1>Hello There</h1>"
-  videoList:[];
+  videoList:any[];
 
   constructor(private videoService:VideoService) {}
   
@@ -24,7 +24,7 @@ export class VideoListComponent implements OnInit,OnDestroy {
 
   this.req=this.videoService.list().subscribe(data=>{
     console.log(data);
-    this.videoList=JSON.parse(data);
+    this.videoList=data;
     
   })
   }
@@ -32,11 +32,13 @@ export class VideoListComponent implements OnInit,OnDestroy {
 
 
   ngOnDestroy(){
-    this.req.unsubscribe();
+    if(this.req){
+      this.req.unsubscribe();
+    }
   }
   
 
-  getEmbedUrl(videoItem){
+  getEmbedUrl(videoItem:any){
     return "https://www.youtube.com/embed/"+videoItem.embed;
     
   }
